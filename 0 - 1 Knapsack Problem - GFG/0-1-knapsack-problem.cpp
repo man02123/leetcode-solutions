fc1008956f72d5893fbe8673143f0995e602ba9c
man02123 <118069900+man02123@users.noreply.github.com>
2023-01-07 22:59:07 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -11,31 +11,47 @@ class Solution
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-       //vector<vector<int>> dp(n+1,vector<int>(W+1,-1));
        memset(dp,-1,sizeof(dp));
-      return help(wt,val,W,n);
+       for(int j=0;j<W+1;j++)
+        dp[0][j]=0;
+        for(int j=0;j<n+1;j++)
+        dp[j][0]=0;
+        
+        for(int i=1;i<n+1;i++)
+         for(int j=1;j<W+1;j++)
+         {
+             if(wt[i-1]<=j)
+             dp[i][j]=max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
+             else
+             dp[i][j]=dp[i-1][j];
+         }
+        
+       
+       
+       
+      return dp[n][W];
      
       
     }
- int help(int wt[],int val[],int cap,int item)
- {
-     if(item==0 || cap==0)
-     return 0;
+//  int help(int wt[],int val[],int cap,int item)
+//  {
+//      if(item==0 || cap==0)
+//      return 0;
      
-     if(dp[item][cap]!=-1)return dp[item][cap];
+//      if(dp[item][cap]!=-1)return dp[item][cap];
      
      
-     if(cap>=wt[item-1])
-     {
-     int x=val[item-1]+help(wt,val,cap-wt[item-1],item-1);
-     int y=help(wt,val,cap,item-1);
-     return dp[item][cap]=max(x,y);
-     }
+//      if(cap>=wt[item-1])
+//      {
+//      int x=val[item-1]+help(wt,val,cap-wt[item-1],item-1);
+//      int y=help(wt,val,cap,item-1);
+//      return dp[item][cap]=max(x,y);
+//      }
      
-     else if(cap<wt[item-1])
-     return dp[item][cap]=help(wt,val,cap,item-1);
+//      else if(cap<wt[item-1])
+//      return dp[item][cap]=help(wt,val,cap,item-1);
          
- }
+//  }
     
 };
 
