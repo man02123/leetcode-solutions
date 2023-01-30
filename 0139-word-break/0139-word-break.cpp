8524ea6d30a10f98e50bc020611d566806201970
man02123 <118069900+man02123@users.noreply.github.com>
2023-01-30 22:09:12 +0530

    Time: 26 ms (33.53%), Space: 14.7 MB (21.50%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int dp[301][301];
+    //int dp[301][301];
     unordered_map<string,int> mp; 
     bool wordBreak(string s, vector<string>& w) {
         for(auto it:w)
             mp[it]=false;
-        memset(dp,-1,sizeof(dp));
+        vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1,-1));
         
-    return solve(s,0,s.size());
+    return solve(s,0,s.size(),dp);
         
         
     }
-    bool solve(string &s,int start,int end)
+    bool solve(string &s,int start,int end,vector<vector<int>> &dp)
     {
         if(start>=end)
             return true;
+        
         if(dp[start][end]!=-1)
             return dp[start][end];
         
@@ -23,7 +24,7 @@ public:
         {
             if(mp.find(s.substr(start,i-start+1))!=mp.end())
             {
-                flag=flag || solve(s,i+1,end);  
+                flag=flag || solve(s,i+1,end,dp);  
             }
         }
         return dp[start][end]= flag;
