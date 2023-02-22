ff7ef20f5ad3daa12e1f31057e8b2c6678b45eb0
man02123 <118069900+man02123@users.noreply.github.com>
2023-02-22 21:49:58 +0530

    Time: 57 ms (48.12%), Space: 22.4 MB (33.60%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -20,10 +20,12 @@ bool check(vector<int>& nums,int div,int t)
     int sum=0;
     for(int i=0;i<nums.size();i++)
     {
-        if(nums[i]%div>0)
-            sum+=(nums[i]/div)+1;
-        else
-            sum+=(nums[i]/div);
+        sum+=ceil((1.0*nums[i])/div);
+        
+        // if(nums[i]%div>0)
+        //     sum+=(nums[i]/div)+1;
+        // else
+        //     sum+=(nums[i]/div);
     }
     if(sum>t)
         return false;
