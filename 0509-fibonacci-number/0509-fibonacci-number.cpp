91575620a4a8b594e3cc3bfed8003a01584a8764
man02123 <118069900+man02123@users.noreply.github.com>
2023-04-10 00:05:02 +0530

    Time: 0 ms (100.00%), Space: 6 MB (21.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,16 +3,15 @@ public:
     int fib(int n) {
         if(n<2)
             return n;
-        
-        int a=0;
-        int b=1;
         int res=0;
         
-        for(int i=1;i<n;i++)
+        int lst=1;
+        int seclst=0;
+        for(int i=2;i<=n;i++)
         {
-            res=a+b;
-            a=b;
-            b=res;
+            res=lst+seclst;
+            seclst=lst;
+            lst=res;
         }
         return res;
     }
