b9abcee65002b5de30a87767b0525ae10f0ac0b8
man02123 <118069900+man02123@users.noreply.github.com>
2023-04-15 16:45:56 +0530

    Added solution - LeetHub

---
+++ Next Greater Element 2 - GFG/next-greater-element-2.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> nextGreaterElement(int n, vector<int>& arr) {
+        stack<int> st;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            if(st.size()==0)
+            st.push(arr[i]);
+            else
+            {
+                while(!st.empty() && arr[i]>=st.top())
+                st.pop();
+                st.push(arr[i]);
+            }
+        }
+        vector<int> res;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            if(st.size()==0)
+            {
+                st.push(arr[i]);
+                res.push_back(-1);
+            }
+            else
+            {
+                while(!st.empty() && arr[i]>=st.top())
+                st.pop();
+                
+                if(st.size()==0)
+                res.push_back(-1);
+                else 
+                res.push_back(st.top());
+                
+                st.push(arr[i]);
+            }
+            
+        }
+        reverse(res.begin(),res.end());
+        return res;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) cin >> arr[i];
+
+        Solution obj;
+        vector<int> ans = obj.nextGreaterElement(N, arr);
+        for (auto &it : ans) {
+            cout << it << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
