d201d7475b56aae228ff642de6f9f2d355e516fe
man02123 <118069900+man02123@users.noreply.github.com>
2023-04-21 21:02:23 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.cpp
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -10,44 +10,44 @@ class Solution
 {
     public:
     //Function to find total number of unique paths.
-    int NumberOfPath(int m, int n)
+    int NumberOfPath(int a, int b)
     {
-    //   int dp[a][b];
-    //   for(int i=0;i<a;i++)
-    //   {
-    //       for(int j=0;j<b;j++)
-    //       {
-    //           if(i==0 || j==0)
-    //           dp[i][j]=1;
-    //           else 
-    //           dp[i][j]=0;
-    //       }
-    //   }
+      int dp[a][b];
+      for(int i=0;i<a;i++)
+      {
+          for(int j=0;j<b;j++)
+          {
+              if(i==0 || j==0)
+              dp[i][j]=1;
+              else 
+              dp[i][j]=0;
+          }
+      }
        
     
        
-    //   for(int i=0;i<a;i++)
-    //   {
-    //       for(int j=0;j<b;j++)
-    //       dp[i][j]=dp[i-1][j]+dp[i][j-1];
-    //   }
-    //   return dp[a-1][b-1];
-     vector<vector<int>> dp(m+1,vector<int> (n+1,0));
-        for(int j=0;j<n;j++)
-            dp[0][j]=1;
+      for(int i=1;i<a;i++)
+      {
+          for(int j=1;j<b;j++)
+          dp[i][j]=dp[i-1][j]+dp[i][j-1];
+      }
+      return dp[a-1][b-1];
+    //  vector<vector<int>> dp(m+1,vector<int> (n+1,0));
+    //     for(int j=0;j<n;j++)
+    //         dp[0][j]=1;
         
-        for(int j=0;j<m;j++)
-            dp[j][0]=1;
-        //dp[0][0]=0;
+    //     for(int j=0;j<m;j++)
+    //         dp[j][0]=1;
+    //     //dp[0][0]=0;
         
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
-            {
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+    //     for(int i=1;i<m;i++)
+    //     {
+    //         for(int j=1;j<n;j++)
+    //         {
+    //             dp[i][j]=dp[i-1][j]+dp[i][j-1];
+    //         }
+    //     }
+    //     return dp[m-1][n-1];
     }
 };
 
