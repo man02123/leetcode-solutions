6e5f42b7dabbd433b25bf24ceaf6b930e6dcbde9
man02123 <118069900+man02123@users.noreply.github.com>
2023-04-22 23:23:37 +0530

    Time: 18 ms (61.79%), Space: 12.7 MB (29.92%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int t) {
-       return help(nums,t,nums.size()-1); 
-    }
-    int help(vector<int>& nums,int t,int n)
-    {
-        if(n<0 && t==0)
-            return 1;
+        int sum=0;
         
-        if(n<0)
+        for(auto it:nums)
+            sum+=it;
+        if((sum+t)%2 !=0 || t+sum<0)
             return 0;
-      return help(nums,t+nums[n],n-1)+help(nums,t-nums[n],n-1);
+        int x=(sum+t)/2;
+        vector<vector<int>> dp(nums.size()+1 ,vector<int> (x+1,-1));
+        
+       return help(nums,(t+sum)/2,nums.size(),dp); 
+    }
+    int help(vector<int>& nums,int t,int n,vector<vector<int>> &dp)
+    {
+        if(n==0 && t==0)
+            return 1;
+        if(n==0)
+            return 0;
+        if(dp[n][t]!=-1)
+            return dp[n][t];
+        
+      if(t-nums[n-1]>=0)
+      {
+          return dp[n][t]=help(nums,t-nums[n-1],n-1,dp)+help(nums,t,n-1,dp);
+      }
+        return dp[n][t]= help(nums,t,n-1,dp);
         
     }
 };
