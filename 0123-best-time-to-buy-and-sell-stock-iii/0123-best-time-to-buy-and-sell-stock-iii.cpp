f4abf63ceac9a0c7f2ddaaa5374fb6e2e8f57910
man02123 <118069900+man02123@users.noreply.github.com>
2023-04-23 20:15:35 +0530

    Time: 220 ms (70.46%), Space: 88.8 MB (62.59%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,27 +1,35 @@
 class Solution {
-public:
+public:int dp[100001][3][2];
     int maxProfit(vector<int>& p) {
-        vector<vector<vector<int>>> dp(p.size(), vector<vector<int>>(2, vector<int>(3, -1))) ;
-        return solve(p,0,2,1,dp);
+       // vector<vector<vector<int>>> dp(p.size(), vector<vector<int>>(2, vector<int>(3, -1))) 
+        memset(dp,-1,sizeof(dp));
+        return recur(p,0,2,1);
     }
     
-    int solve(vector<int> &p,int ind,int tlim,bool canbuy, vector<vector<vector<int>>> &dp)
+    int recur(vector<int> &p,int ind,int k,bool canbuy)
     {
-        if(ind>=p.size() || tlim<0)return 0;
-        
-        if(ind==p.size()-1 && canbuy==false)return p[ind];
-       
-        if(dp[ind][canbuy][tlim]!=-1) return dp[ind][canbuy][tlim];
-        
-        int b=0,s=0;
-        if(canbuy==true)
-        {
-            b=max(-p[ind]+solve(p,ind+1,tlim-1,0,dp),solve(p,ind+1,tlim,1,dp)); 
-        }
-        else
-            s=max(p[ind]+solve(p,ind+1,tlim,1,dp),solve(p,ind+1,tlim,0,dp));
-        
-        return dp[ind][canbuy][tlim]=max(b,s);
+         
+    if(k==0 && canbuy==true)
+    return 0;
+    
+    if(ind>=p.size())
+    return 0;
+    
+    
+    if(dp[ind][k][canbuy]!=-1)
+    return dp[ind][k][canbuy];
+    
+    if(canbuy==true  && k>0)
+    {
+        int by=-p[ind]+recur(p,ind+1,k-1,false);
+        int nby=recur(p,ind+1,k,true);
+        return dp[ind][k][canbuy]=max(by,nby);
+    }
+    
+        int sl=p[ind]+recur(p,ind+1,k,true);
+        int ns=recur(p,ind+1,k,false);
+        return dp[ind][k][canbuy]=max(sl,ns);
+    
         
     }
     
