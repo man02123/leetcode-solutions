ec81b088f42d8dde23f900b67d07c7ae13d1d804
man02123 <118069900+man02123@users.noreply.github.com>
2023-05-17 18:03:24 +0530

    Time: 3 ms (95.18%), Space: 8.3 MB (45.93%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -13,14 +13,34 @@ public:
     ListNode* reverseList(ListNode* head) {
         return help(head);
     }
-    ListNode* help (ListNode* head)
-    {
-        if(head==NULL || head->next==NULL )
-            return head;
+//     ListNode* help (ListNode* head)
+//     {
+//         if(head==NULL || head->next==NULL )
+//             return head;
         
-        ListNode* res=help(head->next);
-          head->next->next=head;
-        head->next=NULL;
-        return res;
+//         ListNode* res=help(head->next);
+//           head->next->next=head;
+//         head->next=NULL;
+//         return res;
+//     }
+    
+    ListNode* help(ListNode* head)
+    {
+        ListNode* fow =NULL ,*prev = NULL;
+        
+        while(head!=NULL)
+        {
+            fow = head->next;
+            head->next = prev;
+            prev = head;
+            head= fow;
+        }
+        return prev;
+    //     while(head!=NULL)
+    //     {
+    //         cout<<head->val;
+    //         head = head->next;
+    //     }
+    // return head;
     }
 };
