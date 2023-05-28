968b8047f32879ed1765ba787206eb5ae17029d1
man02123 <118069900+man02123@users.noreply.github.com>
2023-05-28 17:18:12 +0530

    Time: 18 ms (6.25%), Space: 8.9 MB (43.75%) - LeetHub

---
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string removeTrailingZeros(string num) {
+        while(num.size()>0 && num.back()=='0')
+            num.pop_back();
+        
+        return num;
+        
+    }
+};
