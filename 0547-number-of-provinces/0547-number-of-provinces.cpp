aee45130ff9bb969ea4aea6714d66d458a7c4840
man02123 <118069900+man02123@users.noreply.github.com>
2023-06-03 16:32:21 +0530

    Time: 24 ms (77.01%), Space: 15.1 MB (27.95%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,94 +1,59 @@
-class Disjoint{
-
-vector<int> rank,parent,size;
-public:
-  Disjoint(int n)
-  {
-    rank.resize(n+1,0);
-    size.resize(n+1,0);
-    parent.resize(n+1);
-    for(int i =0;i<=n;i++)
-    {
-      parent[i] = i;
-
-    }
-
-  }
-  int findparent(int node)
-  {
-    if(parent[node] == node)
-      return node;
-    else
-      return parent[node] = findparent(parent[parent[node]]); //pathcompression
-  }
-  void UnionByRank(int u ,int v)
-  {
-    int ult_u = findparent(u);
-    int ult_v = findparent(v);
-
-    if(ult_u == ult_v)
-      return ;
-    if(rank[u] < rank[v])
-    {
-      parent[ult_u] = ult_v;
-    }
-    else if(rank[u] > rank[v])
-    {
-      parent[ult_v] = ult_u;
-    }
-    else
-    {
-      parent[ult_v] = ult_u;
-      rank[ult_u]++;
-    }
-
-  }
-  void UnionBySize(int u ,int v)
-  {
-    int ult_u = findparent(u);
-    int ult_v = findparent(v);
-
-    if(ult_u == ult_v)
-      return ;
-    if(size[ult_v] < size[ult_u])
-    {
-      parent[ult_v]  = ult_u;
-      size[ult_u]+=size[ult_v];
-    }
-    else 
-    {
-      parent[ult_u]  = ult_v;
-      size[ult_v]+=size[ult_u];
-
-    }
-
-  }
-
-};
-
-
-
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& gr){
-        Disjoint ds(gr.size());
+    int findCircleNum(vector<vector<int>>& g) {
         
-        for(int i =0;i<gr.size();i++)
+    int  cnt = 0;
+        int n = g.size();
+        vector<vector<int>> graph(n); 
+        
+        for(int i = 0;i<g.size();i++)
         {
-            for(int j = 0;j<gr.size();j++)
+            for(int j = 0;j<g.size();j++)
             {
-                if(gr[i][j]  == 1)
-                    ds.UnionBySize(i,j);
+                if(g[i][j] ==1  && i!=j)
+                {
+                    graph[i].push_back(j);
+                    graph[j].push_back(i);
+                }
+                    
             }
         }
-        int cnt = 0;
-        for(int i=0;i<gr.size();i++)
+        
+//         for(auto it:graph)
+//         {
+//             for(auto it2:it)
+//                 cout<<it2;
+//             cout<<"\n";
+
+//         }
+        
+        
+        vector<int> vis(n);
+        
+        for(int i = 0;i<n;i++)
         {
-          if(ds.findparent(i) ==i)cnt++;
-            
+            if(vis[i] == 0)
+            {
+                //cout<<i;
+                dfs(graph,i,vis);  
+                cnt++;
+             }
         }
         return cnt;
+  
+    }
+    
+    void dfs( vector<vector<int>> &graph ,  int node , vector<int> &vis)
+    {
+       
+        vis[node] = 1;
         
+        for(auto it:graph[node])
+        {
+            if(vis[it] == 0 )
+                dfs(graph, it,vis);
+        }
         
     }
+            
 };
