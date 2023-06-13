7b1f533605638e5c4c22f7e882a8513c332136d8
man02123 <118069900+man02123@users.noreply.github.com>
2023-06-13 20:50:31 +0530

    Time: 22 ms (75.70%), Space: 13.6 MB (68.44%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& g) {
+    bool canFinish(int n, vector<vector<int>>& pr) {
+        vector<int> g[n];
+        vector<int> indeg(n);
         
-        vector<int> indegree(n,0);
-        vector<vector<int>> graph(n);
-        
-       for(auto it:g)
-       {
-           graph[it[1]].push_back(it[0]);
-           indegree[it[0]]++;
-       }
-        
+        for(auto it:pr){
+            g[it[1]].push_back(it[0]);
+            indeg[it[0]]++; 
+        }
         queue<int> q;
-        for(int i=0;i<n;i++)
-            if(indegree[i]==0)
+        for(auto i = 0;i<n;i++)
+            if(indeg[i] == 0)
                 q.push(i);
-            
-        int count=0;
-        while(q.size()>0)
-        {
-          int node=q.front();
+        
+        while(!q.empty()){
+            int node = q.front();
             q.pop();
-            count++;
-          
-            for(auto it:graph[node])
-            {
-                indegree[it]--;
-                if(indegree[it]==0)
+            
+            for(auto it:g[node]){
+                indeg[it]--;
+                
+                if(indeg[it]==0)
                     q.push(it);
             }
             
-            
         }
-        return (count==n)?1:0;
-            
+        for(auto it:indeg)
+            if(it!=0)return false;
+        
+        return true;
         
     }
 };
