966bdc84cee9d15294cd42fe80f588cc3875bf81
man02123 <118069900+man02123@users.noreply.github.com>
2023-06-19 23:07:35 +0530

    Time: 806 ms (64.39%), Space: 104 MB (92.09%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,49 @@
+#define pi pair<int,pair<int,int>> 
+class Solution {
+public:int n,m;
+    bool isvalid(int x , int y){
+        if(x>=0 && y>=0 && x<m && y<n)
+            return true;
+        return false;
+    }
+    int minimumObstacles(vector<vector<int>>& g) {
+      m = g.size();
+        n = g[0].size();
+        vector<vector<int>> dist(m,vector<int>(n,1e8));
+        dist[0][0] = 0;
+        
+        int dx[4] = {-1,0,1,0};
+        int dy[4] = {0,-1,0,1};
+        
+        priority_queue<pi,vector<pi>,greater<pi>> pq;
+        
+       pq.push({0,{0,0}});
+        while(!pq.empty()){
+            int ob = pq.top().first;
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+            pq.pop();
+           // cout<<x<<y<<" ";
+            for(int i = 0;i<4;i++){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                
+                if(isvalid(nx,ny)){
+                  if(g[nx][ny] == 1  && dist[nx][ny]>ob+1)
+                  {
+                      dist[nx][ny] = 1+ob;
+                      pq.push({dist[nx][ny],{nx,ny}});
+                  }
+                    else if( g[nx][ny] == 0  && dist[nx][ny]>ob)
+                    {
+                        dist[nx][ny] = ob;
+                        pq.push({dist[nx][ny],{nx,ny}});
+                    }
+                }
+            }
+  
+        }
+         return dist[m-1][n-1];
+        
+    }
+};
