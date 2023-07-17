6bbc038c9cea170f4617f2db28c2bd70734bcfac
man02123 <118069900+man02123@users.noreply.github.com>
2023-07-17 19:32:47 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (60.63%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-       vector<vector<int>> res;
-        vector<int> temp;
-       return recur(nums,0,res,temp);
-        
+        vector<vector<int>> res;
+        int n = nums.size();
+        for(int i = 0;i < (1<<n) ;i++)
+           solve(res,i , nums);
+
+        return res;
         
     }
-   vector<vector<int>> recur(vector<int>& nums,int ind,vector<vector<int>> &res,vector<int>&temp)
-   {
-       
-       if(ind==nums.size())
-       {
-           res.push_back(temp);
-           return res;
-       }
-       temp.push_back(nums[ind]);
-       recur(nums,ind+1,res,temp);
-       temp.pop_back();
-       recur(nums,ind+1,res,temp);
-       return res;
-   }
+    void solve(vector<vector<int>>&ans , int mask , vector<int> &nums)
+    { 
+       int j = 0;
+       vector<int> t;
+            while(mask > 0)
+            {
+                if((mask&1) == 1)
+                {
+                 t.push_back(nums[j]);
+                }
+                j++;
+               mask= mask>>1;
+            }
+            
+            ans.push_back(t);
+        return;
+    }
+    
 };
+
+
