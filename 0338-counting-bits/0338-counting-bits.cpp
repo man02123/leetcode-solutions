f71cf7570d32e96db70bc04544fc97babba38f43
man02123 <118069900+man02123@users.noreply.github.com>
2023-09-01 15:33:49 +0530

    Time: 3 ms (89.82%), Space: 8.8 MB (5.98%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,19 +2,26 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> res;
-        res.push_back(0);
-        for(int i = 1;i<=n ;i++){
-            res.push_back(solve(i));
+       // res.push_back(0);
+        int dp[n+1];
+        memset(dp,-1,sizeof(dp));
+        
+        for(int i = 0;i<=n ;i++){
+            res.push_back(solve(i,dp));
         }
         return res;
     }
-    int solve(int num){
+    int solve(int num , int dp[]){
+        if(num==0)
+            return 0;
         if(num == 1)
             return 1;
+        if(dp[num] != -1)
+            return dp[num];
         
         if(num%2 == 0)
-            return solve(num/2);
+            return dp[num] = solve(num/2,dp);
         else
-            return 1+solve(num/2);
+            return dp[num] = 1+solve(num/2,dp);
     }
 };
