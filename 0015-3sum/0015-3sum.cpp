b006102437c9d424a501d5a267a0a3c09b77d95c
man02123 <118069900+man02123@users.noreply.github.com>
2023-09-04 19:09:21 +0530

    Time: 92 ms (93.05%), Space: 24.1 MB (65.47%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
+        sort(nums.begin() , nums.end());
+        int n = nums.size();
         
-        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
         
-        for(int i =0;i<=nums.size()-3;i++)
-        {
-            
-            
+        for(int i = 0;i<nums.size();i++){
             while(i>0 && nums[i] == nums[i-1] && i<=nums.size()-3)
                 i++;
-            
-            int l =i+1,r = nums.size()-1;
-            
-            while(l<r)
-            {
-               if(nums[i] +nums[l]+nums[r] ==0)
-               {
-                res.push_back({nums[i] ,nums[l],nums[r]});
-                    
-                   while(l<r && nums[l]==nums[l+1])
-                    l++;
-                   
-                    while(r>l && nums[r] == nums[r-1])
-                    r--;
-                   
-                   l++;
-                   r--;
-               }
-                else if(nums[l]+nums[r]+nums[i]<0)
-                    l++;
-                else
-                    r--;
+            int num = nums[i];
+            int l = i+1;
+            int r = n-1;
+            long long csum = num;
+            while(l<r){
+                csum  = num + nums[l]+nums[r];
                 
+                if(csum == 0){
+                    ans.push_back({num,nums[l],nums[r]});
+               
+                while(l<r && nums[l+1] == nums[l] )
+                    l++;
+                
+                while(l<r && nums[r-1] == nums[r])
+                    r--;
+                    
+                  r--;
+                }
+                
+                else if(csum<0)
+                l++;
+                else r--;
+                
+                    
+                    
             }
-            
-            
         }
-        return res;
-        
+        return ans;
     }
 };
