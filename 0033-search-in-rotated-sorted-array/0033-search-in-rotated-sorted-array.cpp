f3b168d2e7af500a8fd37caa0eb9e1327d13d39c
man02123 <118069900+man02123@users.noreply.github.com>
2023-10-01 19:31:10 +0530

    Time: 3 ms (64.95%), Space: 11.3 MB (9.38%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,32 +1,75 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int tar) {
-        int l = 0,r =nums.size()-1;
+    int search(vector<int>& nums, int t) {
+        int ind = -1;
         
-        while(l<=r)
-        {
-            int mid = l+(r-l)/2;
-            if(nums[mid] == tar)
-                return mid;
+        int l = 0 , r = nums.size()-1;
+        
+        while(l<r){
+            int mid = (l)+(r-l)/2;
             
-            if(nums[l]<=nums[mid])
-            {
-                if(nums[l]<=tar && nums[mid]>tar)
-                {
-                    r = mid-1;
-                }
-                else
-                    l = mid+1;
+            if(nums[mid] > nums[r]){
+                ind = mid+1;
+                l = mid+1;
             }
-            else 
-            {
-                if(nums[mid]<=tar && tar<=nums[r])
-                    l=mid+1;
+            else
+                r = mid;
+        }
+        
+        
+        if(ind !=-1 && t >= nums[ind] && t<= nums.back()){
+            l = ind,r = nums.size()-1;
+            while(l<=r){
+                int mid = (l)+(r-l)/2;
+            
+            if(nums[mid] == t){
+                return mid;
+            }
+            else if(nums[mid] < t)
+                l = mid+1;
                 else
-                    r=mid-1;
+                    r = mid-1;
                 
             }
+            return -1;
         }
+        else if(ind!=-1){
+            l = 0,r = ind-1;
+            while(l<=r){
+                int mid = (l)+(r-l)/2;
+            
+            if(nums[mid] == t){
+                return mid;
+            }
+            else if(nums[mid] < t)
+                l = mid+1;
+                else
+                    r = mid-1;
+                
+            }
+            return -1;
+            
+        }
+        else{
+            l = 0,r = nums.size()-1;
+            while(l<=r){
+                int mid = (l)+(r-l)/2;
+            
+            if(nums[mid] == t){
+                return mid;
+            }
+            else if(nums[mid] < t)
+                l = mid+1;
+                else
+                    r = mid-1;
+                
+            }
+            return -1;
+            
+            
+        }
+        
+        
         return -1;
         
     }
